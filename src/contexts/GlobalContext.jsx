43a7b262368424ae69598bb6b5e99c836939ea7b
Danilo-Mosca <danilo.mosca85@gmail.com>
2025-01-24 12:47:52 +0100

    Global Context

---
+++ src/contexts/GlobalContext.jsx
@@ -0,0 +1,28 @@
+// Creazione della GlobalContext che conterrà tutte le chiamate API al server
+import { createContext } from "react";
+import axios from "axios";
+import { use } from "react";
+
+const GlobalContext = createContext();  //creo il Context e gli do il nome GlobalContext
+
+// Creo il provider customizzato:
+const GlobalProvider = ({ children }) => {
+}
+
+// Oggetto contenente i dati da passare al value per offrirli ai Consumer (i componenti racchiusi nel Provider di GLobalContext):
+const collectionData = {
+
+}
+
+// Creo una hook personalizzata per accedere al Context:
+function useGlobalContext() {
+    const context = useContext(GlobalContext);
+
+    // Se per sbaglio non dovessi inserire correttamente il Provider nel file App.jsx, allora genero un errore per facilitare il debug:
+    if (!context) {
+        throw new Error("useGlobalContext is not inside the context provider GlobalProvider");
+    }
+    return context;
+}
+
+export { GlobalProvider, useGlobalContext };
