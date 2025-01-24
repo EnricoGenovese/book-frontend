0a77674b6855dba0ef733d532c56a5658e1b5512
DavidIncarbone <david.incarbone@gmail.com>
2025-01-24 14:33:49 +0100

    fixed Bool Books text

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,13 +5,10 @@ export default function Navbar() {
     return (
 
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
-
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                     <li>
-                        <NavLink to="/" className="nav-link" style={({ isActive }) => {
-                            return isActive ? { color: "red" } : {}
-                        }} end><h5>Bool Books</h5></NavLink>
+                        <NavLink to="/" className="nav-link" ><h5>Bool Books</h5></NavLink>
                     </li>
                     <li className="nav-item">
                         <NavLink to="/books" className="nav-link active" aria-current="page" style={({ isActive }) => {
