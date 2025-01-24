f769670a983d721b3e0592b07e46807305f5f6ae
EnricoGenovese <enricogeno@libero.it>
2025-01-24 16:56:41 +0100

    done api test;

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,13 @@
 
 import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { GlobalProvider } from "./contexts/GlobalContext.jsx"
+
 import DefaultLayout from "./pages/DefaultLayout.jsx"
 import HomePage from "./pages/Homepage.jsx"
 import About from "./pages/About.jsx"
 import Contacts from "./pages/Contacts.jsx"
 import Books from "./pages/Books.jsx"
 import Details from "./pages/Details.jsx"
-
-import Header from "./components/Header"
-import { GlobalProvider } from "./contexts/GlobalContext.jsx"
-
 
 function App() {
 
--- src/pages/Books.jsx
+++ src/pages/Books.jsx
@@ -1,17 +1,17 @@
-import { useGlobalContext } from "../contexts/GlobalContext"
-import Card from "../components/Card.jsx"
+import { useGlobalContext } from "../contexts/GlobalContext";
+import Card from "../components/Card.jsx";
 
 export default function Books() {
-    const { books } = useGlobalContext()
+    const { books } = useGlobalContext();
     return (
-        <section className="container py-4 mx-4">
-            <div className="row">
-                <div className="col-12 col-md-4 col-lg-3">
-                    {books.map((book) => {
-                        <Card data={book} key={book.id} />
-                    })}
-                </div>
+        <section className="container py-4">
+            <div className="row g-3">
+                {books.map((book) => (
+                    <div className="col-12 col-md-6 col-lg-4" key={book.id}>
+                        <Card data={book} />
+                    </div>
+                ))}
             </div>
         </section>
-    )
+    );
 }
