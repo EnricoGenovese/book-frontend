d1de2d1d15060d693819d87135bf974200586f59
AgostinoCaruso <agostinocaruso321@gmail.com>
2025-01-24 14:29:17 +0100

    fix card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,11 +1,12 @@
 function Card({ data }) {
+    const imgPath = "http://localhost:3000/img/books" + data.image;
     return (
         <>
             <div className=" container">
                 <div className=" row row-col-3">
                     {data.map((ele) => {
                         <div className="card col">
-                            <img className="card-img-top" src={ele.image} alt={ele.title} />
+                            <img className="card-img-top" src={imgPath} alt={ele.title} />
                             <div className="card-body">
                                 <h5 className="card-title">{ele.title}</h5>
                                 <h5 className="card-title">{ele.author}</h5>
