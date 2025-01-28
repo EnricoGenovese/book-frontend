d7dfd886ec1c6e90e6d7f08032e54a4add35d9fc
AgostinoCaruso <agostinocaruso321@gmail.com>
2025-01-28 19:25:52 +0100

    semi fix contact

--- src/pages/Contacts.jsx
+++ src/pages/Contacts.jsx
@@ -1,12 +1,10 @@
-
-
 export default function Contacts() {
     return (
         <section className="container">
             <h1>Contattaci su questi canali</h1>
-            <div className="d-flex">
-                <div className="row justify-content-center">
-                    <div className="my-5 col-12 cold-md-6 col-lg-4">
+            <div className="d-flex justify-content-around">
+                <div className="row justify-content-center w-100">
+                    <div className="my-5 col-12 col-md-6 col-lg-4">
                         <div className="my-3">
                             <h2>Via Enzo ed Elvira Sellerio 50, 90141 Palermo - Italia</h2>
                             <p>Tel. (+39) 091.625.4194</p>
@@ -26,7 +24,7 @@ export default function Contacts() {
                             <p>ufficiostampa@sellerio.it</p>
                         </div>
                     </div>
-                    <div className="my-5 col-12 cold-md-6 col-lg-4">
+                    <div className="my-5 col-12 col-md-6 col-lg-4">
                         <div className="my-3">
                             <h2>Distribuzione</h2>
                             <p>Messaggerie Libri S.p.A.</p>
@@ -37,7 +35,7 @@ export default function Contacts() {
                             <p>www.messaggerielibri.it</p>
                         </div>
                     </div>
-                    <div className="my-5 col-12 cold-md-6 col-lg-4">
+                    <div className="my-5 col-12 col-md-6 col-lg-4">
                         <div className="my-3">
                             <h2>Propaganda</h2>
                             <p>Promedi S.r.l.</p>
@@ -49,6 +47,6 @@ export default function Contacts() {
                     </div>
                 </div>
             </div>
-        </section >
-    )
+        </section>
+    );
 }
