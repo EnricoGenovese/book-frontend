0d9bd0a0c2bca89d17411e682f5d1cc7f4cc56f3
AgostinoCaruso <agostinocaruso321@gmail.com>
2025-01-28 17:02:24 +0100

    fix bookdetails

--- src/components/BookDetails.jsx
+++ src/components/BookDetails.jsx
@@ -1,56 +1,48 @@
-import { useParams, Link } from "react-router-dom";
-import { useEffect } from "react";
+// Hook usato per accedere a rotte dinamiche (consente di estrarre i parametri dalla URL, in questo caso mi serve il parametro dell'id):
+
+import { useParams } from "react-router-dom"
+import { Link } from "react-router-dom";
+import axios from "axios";
 import Star from "./Star";
 import Loader from "./Loader";
-import style from "./BookDetails.module.css";
+// Importo il CSS Modules di BooksDetails
+import style from "./BookDetails.module.css"
 import ReviewForm from "./ReviewForm";
 
-
 export default function BookDetails({ dataBooks }) {
-    const { id } = useParams(); // Ricavo l'id dalla URL
+    const { id } = useParams();     //Destrutturo useParames e ricavo l'id
+    console.log("dataBooks: ", dataBooks);
     const imgPath = "http://localhost:3000/img/";
-
-    // Resetta lo scroll ogni volta che cambia l'id del libro
-    useEffect(() => {
-        window.scrollTo(0, 0); // Porta la scrollbar in cima
-    }, ["books/:id"]); // L'effetto viene eseguito ogni volta che cambia il parametro `id`
 
     return (
         <>
-            {dataBooks ? (
+            {dataBooks ?
                 <>
                     <div className="col-12 col-md-6 col-lg-4" key={id}>
                         <div className="d-flex mb-5">
                             <img className="card-img-top me-5 w-50" src={imgPath + dataBooks.image} alt={dataBooks.title} />
 
-                            <div className="d-flex flex-column justify-content-between">
-                                <div className="card-detail-review">
+                            <div className=" d-flex flex-column justify-content-between">
+                                <div className=" card-detail-review">
                                     <h5 className="fs-1">{dataBooks.title}</h5>
-                                    <h5 className="text-secondary">By {dataBooks.author}</h5>
+                                    <h5 className=" text-secondary">By {dataBooks.author}</h5>
                                     <p className="text-secondary">{dataBooks.abstract}</p>
+
                                 </div>
-                                {/* <Link to="/books/" className="btn btn-primary w-25 small-text">
-                                    Torna alla lista dei film
-                                </Link> */}
+                                {/* <Link to='/books/' className="btn btn-primary w-25 small-text">Torna alla lista dei film</Link> */}
                             </div>
                         </div>
                     </div>
-                    <section>
-                        <div className="d-flex justify-content-between">
-                            <h3>Recensioni:</h3>
-                            <h3>
-                                Media voto: <Star num={dataBooks.vote_average} />
-                            </h3>
-                        </div>
+                    <section className="">
+                        <div className=" d-flex justify-content-between "><h3 >Recensioni:</h3><h3 >Media voto: {<Star num={dataBooks.vote_average} />}</h3></div>
+
 
                         {dataBooks.reviews.map((review) => (
                             <div key={review.id}>
                                 <div className={"card d-flex flex-column mb-3"}>
                                     <div className="card-body">
                                         <p className="card-text">{review.text}</p>
-                                        <h5 className="card-title">
-                                            Vote: <Star num={review.vote} />
-                                        </h5>
+                                        <h5 className="card-title">Vote: {<Star num={review.vote} />}</h5>
                                         <p className={`card-text ${style["text-name"]}`}>By {review.name}</p>
                                     </div>
                                 </div>
@@ -61,9 +53,8 @@ export default function BookDetails({ dataBooks }) {
                         <ReviewForm book_id={id} />
                     </section>
                 </>
-            ) : (
-                <Loader />
-            )}
+                : <Loader />
+            }
         </>
     );
 }
