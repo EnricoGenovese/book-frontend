2e5aa5a6b5da003f064c60a937c9d99b2c1e5f0c
EnricoGenovese <enricogeno@libero.it>
2025-01-29 17:36:03 +0100

    pagination button color css;

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -1,14 +1,14 @@
 import { useGlobalContext } from "../contexts/GlobalContext"
-
+import style from "./Pagination.module.css"
 export default function Pagination() {
     const { handlePageChange, page, numPages } = useGlobalContext();
 
     return (
-        <nav className="col-12">
+        <nav className="col-12 d-flex justify-content-center">
             <ul className="pagination">
-                <li className={`page-item ${page <= 1 ? "disabled" : ""}`}>
+                <li className={`page-item ${page <= 1 ? "disabled" : ""} px-5 `}>
                     <button
-                        className="page-link"
+                        className={`page-link ${style.pageBtn}`}
                         onClick={() => handlePageChange(page - 1)}
                     >
                         Previous
@@ -16,27 +16,27 @@ export default function Pagination() {
                 </li>
                 {new Array(numPages).fill("").map((num, index) => (
                     <li
-                        className={`page-item ${page === index + 1 ? "active" : ""} `}
+                        className={`page-item ${page === index + 1 ? "" : ""} `}
                         aria-current="page"
                         key={index + 1}
                     >
                         <button
-                            className="page-link"
+                            className={`page-link ${page === index + 1 ? `${style.pageBtn}` : ""}`}
                             onClick={() => handlePageChange(index + 1)}
                         >
                             {index + 1}
                         </button>
                     </li>
                 ))}
-                <li className={`page-item ${page >= numPages ? "disabled" : ""}`}>
+                <li className={`page-item ${page >= numPages ? "disabled" : ""} px-5`}>
                     <button
-                        className="page-link"
+                        className={`page-link ${style.pageBtn}`}
                         onClick={() => handlePageChange(page + 1)}
                     >
                         Next
                     </button>
                 </li>
             </ul>
-        </nav>
+        </nav >
     )
 }
